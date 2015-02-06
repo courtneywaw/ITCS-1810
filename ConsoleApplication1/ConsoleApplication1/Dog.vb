376c874785a3178c0e6661e7a3d2347016985398
courtneywaw <cwawrowski1@mail.lakelandcc.edu>
2015-02-05 19:10:36 -0500

    Lab 2

---
+++ ConsoleApplication1/ConsoleApplication1/Dog.vb
@@ -0,0 +1,7 @@
+﻿Public Class Dog
+    Public BFF As Dog
+    Public name As String
+    Public breed As String
+    Public Age As Integer
+
+End Class
---
+++ ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim charlie As Dog
+        Dim stuart As Dog
+
+        charlie = New Dog
+        stuart = New Dog
+
+        charlie.name = "charles"
+        charlie.breed = "poodle"
+        charlie.Age = 17
+        charlie.BFF = stuart
+
+        stuart.name = "stuart"
+        stuart.breed = "golden retriever"
+        stuart.Age = 8
+        stuart.BFF = charlie
+
+        Dim youngest As Dog = stuart
+    End Sub
+
+End Module
